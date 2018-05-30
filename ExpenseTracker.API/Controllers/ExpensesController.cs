6632f108d290c42b692cdfb465d5f35912f2fb79
Kevin Gagnon <keving@kultera.com>
2018-05-30 08:40:59 -0400

    Including certain fields in the call only.

--- ExpenseTracker.API/Controllers/ExpensesController.cs
+++ ExpenseTracker.API/Controllers/ExpensesController.cs
@@ -1,4 +1,5 @@
-﻿using ExpenseTracker.Repository;
+﻿using ExpenseTracker.API.Helpers;
+using ExpenseTracker.Repository;
 using ExpenseTracker.Repository.Factories;
 using Marvin.JsonPatch;
 using System;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Http;
+using System.Web.Http.Routing;
 
 namespace ExpenseTracker.API.Controllers
 {
@@ -16,6 +18,8 @@ namespace ExpenseTracker.API.Controllers
 
         IExpenseTrackerRepository _repository;
         ExpenseFactory _expenseFactory = new ExpenseFactory();
+
+        const int maxPageSize = 10;
 
         public ExpensesController()
         {
@@ -27,29 +31,89 @@ namespace ExpenseTracker.API.Controllers
             _repository = repository;
         }
 
-        //api/expensegroups/1/expenses
-        [Route("expensegroups/{expenseGroupId}/expenses")]
-        public IHttpActionResult Get(int expenseGroupId)
+
+        [Route("expensegroups/{expenseGroupId}/expenses", Name = "ExpensesForGroup")]
+        public IHttpActionResult Get(int expenseGroupId, string sort = "date", string fields = null
+            , int page = 1, int pageSize = maxPageSize)
         {
             try
             {
-                var expenses = _repository.GetExpenses(expenseGroupId);
-                if(expenses == null)
+                List<string> lstOfFields = new List<string>();
+                if (fields != null)
                 {
+                    lstOfFields = fields.ToLower().Split(',').ToList();
+                }
+
+                var expenses = _repository.GetExpenses(expenseGroupId);
+
+                if (expenses == null)
+                {
+                    // this means the expensegroup doesn't exist
                     return NotFound();
                 }
 
+                // ensure the page size isn't larger than the maximum.
+                if (pageSize > maxPageSize)
+                {
+                    pageSize = maxPageSize;
+                }
+
+                // calculate data for metadata
+                var totalCount = expenses.Count();
+                var totalPages = (int)Math.Ceiling((double)totalCount / pageSize);
+
+                var urlHelper = new UrlHelper(Request);
+
+                var prevLink = page > 1 ? urlHelper.Link("ExpensesForGroup",
+                    new
+                    {
+                        page = page - 1,
+                        pageSize = pageSize,
+                        expenseGroupId = expenseGroupId,
+                        fields = fields,
+                        sort = sort
+                    }) : "";
+                var nextLink = page < totalPages ? urlHelper.Link("ExpensesForGroup",
+                    new
+                    {
+                        page = page + 1,
+                        pageSize = pageSize,
+                        expenseGroupId = expenseGroupId,
+                        fields = fields,
+                        sort = sort
+                    }) : "";
+
+
+                var paginationHeader = new
+                {
+                    currentPage = page,
+                    pageSize = pageSize,
+                    totalCount = totalCount,
+                    totalPages = totalPages,
+                    previousPageLink = prevLink,
+                    nextPageLink = nextLink
+                };
+
+                HttpContext.Current.Response.Headers.Add("X-Pagination",
+                Newtonsoft.Json.JsonConvert.SerializeObject(paginationHeader));
+
+
                 var expensesResult = expenses
+                    .ApplySort(sort)
+                    .Skip(pageSize * (page - 1))
+                    .Take(pageSize)
                     .ToList()
-                    .Select(exp => _expenseFactory.CreateExpense(exp));
+                    .Select(exp => _expenseFactory.CreateDataShapedObject(exp, lstOfFields));
 
                 return Ok(expensesResult);
+
             }
             catch (Exception)
             {
                 return InternalServerError();
             }
         }
+
 
         [Route("expensegroups/{expenseGroupId}/expenses/{id}")]
         [Route("expenses/{id}")]
@@ -58,6 +122,7 @@ namespace ExpenseTracker.API.Controllers
             try
             {
                 Repository.Entities.Expense expense = null;
+
                 if (expenseGroupId == null)
                 {
                     expense = _repository.GetExpense(id);
@@ -65,7 +130,9 @@ namespace ExpenseTracker.API.Controllers
                 else
                 {
                     var expensesForGroup = _repository.GetExpenses((int)expenseGroupId);
-                    if(expensesForGroup != null)
+
+                    // if the group doesn't exist, we shouldn't try to get the expenses
+                    if (expensesForGroup != null)
                     {
                         expense = expensesForGroup.FirstOrDefault(eg => eg.Id == id);
                     }
@@ -80,6 +147,7 @@ namespace ExpenseTracker.API.Controllers
                 {
                     return NotFound();
                 }
+
             }
             catch (Exception)
             {
