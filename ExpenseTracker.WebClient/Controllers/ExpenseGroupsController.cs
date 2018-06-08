aa6ba3193694445f42de8dca436c923fc63beb61
Kevin Gagnon <klg73@outlook.com>
2018-06-08 15:05:42 -0400

    Created the Editable Expense Group

--- ExpenseTracker.WebClient/Controllers/ExpenseGroupsController.cs
+++ ExpenseTracker.WebClient/Controllers/ExpenseGroupsController.cs
@@ -54,7 +54,7 @@ namespace ExpenseTracker.WebClient.Controllers
             return View(model);
         }
 
- 
+
         // GET: ExpenseGroups/Details/5
         public ActionResult Details(int id)
         {
@@ -62,7 +62,7 @@ namespace ExpenseTracker.WebClient.Controllers
         }
 
         // GET: ExpenseGroups/Create
- 
+
         public ActionResult Create()
         {
             return View();
@@ -96,27 +96,64 @@ namespace ExpenseTracker.WebClient.Controllers
                     return Content("An error has occurred.");
                 }
             }
-            catch 
+            catch
             {
                 return Content("An error has occurred");
             }
         }
 
         // GET: ExpenseGroups/Edit/5
- 
-        public ActionResult Edit(int id)
+
+        public async Task<ActionResult> Edit(int id)
         {
-            return View();
+            var client = ExpenseTrackerHttpClient.GetClient();
+
+            var response = await client.GetAsync("api/expensegroups/" + id);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                var model = JsonConvert
+                    .DeserializeObject<ExpenseGroup>(content);
+
+                return View(model);
+            }
+
+            return Content("An error has occurred.");
+
         }
 
         // POST: ExpenseGroups/Edit/5   
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, ExpenseGroup expenseGroup)
+        public async Task<ActionResult> Edit(int id, ExpenseGroup expenseGroup)
         {
-            return View();
+            try
+            {
+                var client = ExpenseTrackerHttpClient.GetClient();
+             
+                var serializedItemToUpdate = JsonConvert.SerializeObject(expenseGroup);
+
+                var response = await client.PutAsync("api/expensegroups/" + id,
+                    new StringContent(serializedItemToUpdate,
+                        System.Text.Encoding.Unicode,
+                        "application/json"));
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return Content("An error has occurred.");
+                }
+            }
+            catch
+            {
+                return Content("An error has occurred");
+            }
         }
-         
+
 
         // POST: ExpenseGroups/Delete/5
         public ActionResult Delete(int id)
