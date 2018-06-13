62e8c1f4a8e6ea596c144f0c3fefc74a99848e20
Kevin Gagnon <klg73@outlook.com>
2018-06-13 08:05:25 -0400

    Added ThinkTecture Identity Server

---
+++ ExpenseTracker.IdSrv/Config/Clients.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Thinktecture.IdentityServer.Core.Models;
+
+namespace ExpenseTracker.IdSrv.Config
+{
+    public static class Clients
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new[]
+            {
+                new Client
+                {
+                    Enabled = true,
+                    ClientName = "ExpenseTracker MVC Client (Hybrid Flow)",
+                    ClientId = "mvc",
+                    Flow = Flows.Hybrid,
+                    RequireConsent = true
+                }
+            };
+        }
+    }
+}
